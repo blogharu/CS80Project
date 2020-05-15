266a6fb31c7fabb9bc4eca5a5e2fd1a06838cc57
blogharu <34163093+blogharu@users.noreply.github.com>
2020-05-15 02:04:03 -0700

    Update mathFunctions.py
    Add more comments.

--- CS80Project/mathFunctions.py
+++ CS80Project/mathFunctions.py
@@ -3,6 +3,9 @@ import numpy as np
 import random
 
 # Regression
+
+# Get c vector that makes listSamples dot c = listResults smallest. 
+
 def leastSquare(listSamples, listResults, numVariables):
     # Change the listSamples and listResults to matrix
     matrixSamples = np.matrix(listSamples)
@@ -28,8 +31,13 @@ def leastSquare(listSamples, listResults, numVariables):
 
 
 # Helping functions
+
+#####################
+
 def optimizationResultNPArrayToCSVString(result):
     return removeSpaceFromString(np.array2string(result, precision=2, separator=',',suppress_small=True))[1:-1]
+
+# Check if the given string is number or not.
 
 def isNumber(str):
     try:
@@ -38,12 +46,23 @@ def isNumber(str):
     except:
         return False
 
+# Remove space from given string.
+    
 def removeSpaceFromString(str):
     temp = ""
     for char in str:
         if char != ' ':
             temp = temp + char
     return temp
+
+# Change equation string or inequality string to below list.
+#   input        output
+# f(X) = Y  -> [0, f(X)-(Y)]
+# f(X) > Y  -> [1, f(X)-(Y)]
+# f(X) >= Y -> [1, f(X)-(Y)]
+# f(X) < Y  -> [1, Y-(f(X))]
+# f(X) <= Y -> [1, Y-(f(X))]
+# The first element is the type of the equation and second element is eqaution for optimization.
 
 def removeEqaulSign(equation):
     type = 0
@@ -67,6 +86,8 @@ def removeEqaulSign(equation):
     result = result + "(" + temp + ")"
     return [type, result]
 
+# From the given string equation, find the variables in varDicID.
+
 def countNumVariablesInEquation (equation, varDicID):
     countDic = {}
     for key in varDicID.keys():
@@ -83,17 +104,20 @@ def countNumVariablesInEquation (equation, varDicID):
     return countDic
 
 
-# Analize Equation
+# Calculate the equation string.
 def calculateEquationReturnString(x, equation, varDic):
     temp = ""
     temp = removeParenthesis(x, equation, varDic)
     temp = removeMath(x, temp, varDic)
-    #temp = removePower(x, temp, varDic)
+    #temp = removePower(x, temp, varDic) # THIS FUNCTION IS NOT READY YET.
     temp = removeVariablesFromString(x, temp, varDic)
     return str(eval(temp))
 
+# This is for debuging calculateEquationReturnString function.
 printCalculationSteps = 0
 
+# This function is part of the calculateEquationReturnString function.
+# It remove the parenthesis from equation string.
 def removeParenthesis(x, equation, varDic):
     temp = ""
     result = ""
@@ -121,7 +145,8 @@ def removeParenthesis(x, equation, varDic):
     result = result + temp
     return result
 
-##################################################
+# This function is part of the calculateEquationReturnString function.
+# From simpleEquation, find math.sin and math.cos string and change it to number.
 def removeMath(x, simpleEquation, varDic):
     temp = ""
     arg = ""
@@ -154,9 +179,9 @@ def removeMath(x, simpleEquation, varDic):
         if check == 0:
             equation = equation + str(math.cos(float(temp)*math.pi/180))
     return equation
-##################################################
 
 ##################################################
+# This function is ready yet.
 def removePower(x, equation, varDic):
     if printCalculationSteps == 1:
         print("removePower!")
@@ -164,6 +189,9 @@ def removePower(x, equation, varDic):
     return equation
 ##################################################
 
+# This function is part of the calculateEquationReturnString function.
+# Find the variable from simpleEquation and change it to the x[varID].
+# It allow the program to calculate the equation like Xc = Y.
 def removeVariablesFromString(x, simpleEquation, varDicID):
     temp = ""
     equation = ""
@@ -190,10 +218,12 @@ def removeVariablesFromString(x, simpleEquation, varDicID):
     return equation
 
 # Optimization
+
+# Get the random number X such that bound[0] <= X <= bound[1]+bound[0]
 def getRandomNumber(bound):
     return bound[0] + (bound[1]-bound[0])*random.random()
 
-
+# This function is generating objective function for random sample generation.
 def getObjectiveFunctionForRandomGeneration(initial):
     def objective(x):
         result = 0
@@ -202,6 +232,7 @@ def getObjectiveFunctionForRandomGeneration(initial):
         return result
     return objective
 
+# This function is generating objective function for optimization.
 def getObjectiveFunction(npMatrix):
     def objective(x):
         result = 0
@@ -211,11 +242,13 @@ def getObjectiveFunction(npMatrix):
         return result
     return objective
 
+# This function is generating function that is used for optimization constraints.
 def getConstraintFunction(constraintEquation,varDicID):
     def constraint(x):
         return float(calculateEquationReturnString(x,constraintEquation,varDicID))
     return constraint
 
+# This function is generating list of functions that are used for optimization constraints.
 def getConstraintsForOptimization(constraintsList, varDicID):
     result = []
     for constraint in constraintsList:
